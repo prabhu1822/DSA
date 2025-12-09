2f9ffc47130fcf3d9ac9fc71576be935f99859e6
prabhu1822 <gadekarprabhu1822@gmail.com>
2025-12-09 19:48:44 +0530

    Star Patterns

---
+++ WarmUp/starPattern2.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 
+    *
+    *   *
+    *   *   *   
+    *   *   *   *
+  
+  
+ * 
+ */
+
+function starPattern(n){
+    for(let i=0;i<n;i++){
+        let row=" ";
+        for(let j=0;j<=i;j++){
+            row = row + "* ";
+
+        }
+        console.log(row);
+    }
+}
+
+starPattern(4);
